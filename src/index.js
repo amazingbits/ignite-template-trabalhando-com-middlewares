c8f03a7f96e0a1c94e10118c6d3095d98f7927ea
Guilherme Andrade <gui-keanne@hotmail.com>
2022-03-31 13:10:19 -0300

    finalizando desafio - todos os testes passando

--- src/index.js
+++ src/index.js
@@ -10,19 +10,52 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const findUser = users.find((user) => user.username === username);
+  if (!findUser) {
+    return response.status(404).json({ error: "User not found!" });
+  }
+  request.user = findUser;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  if (
+    !user.pro && user.todos.length >= 10
+  ) {
+    return response.status(403).json({ error: "You must upgrade to Pro plan." });
+  }
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+  const user = users.find((user) => user.username === username);
+  if (!user) {
+    return response.status(404).json({ error: "User not found!" });
+  }
+  if (!validate(id)) {
+    return response.status(400).json({ error: "ID must be an UUID" });
+  }
+  const todo = user.todos.find((todo) => todo.id === id);
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found!" });
+  }
+  request.user = user;
+  request.todo = todo;
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const findUser = users.find((user) => user.id === id);
+  if (!findUser) {
+    return response.status(404).json({ error: "User not found" });
+  }
+  request.user = findUser;
+  return next();
 }
 
 app.post('/users', (request, response) => {
